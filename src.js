88b8f8d0d7934aac32004e83ca2218cd8af21be3
7starmohit <67998164+7starmohit@users.noreply.github.com>
2025-07-01 18:46:06 +0530

    Add files via upload
    javaScript callback

---
+++ src.js
@@ -0,0 +1,14 @@
+/*let arr = [1,2,3,4,5];
+arr.forEach((num)=>{
+    console.log(num);
+})*/
+
+/*let arr = ["boby","mumbai","kolkata","bengalore"];
+arr.forEach((val,idx,arr)=>{
+    console.log(val.toUpperCase(),idx,arr);
+})*/
+let arr = [1,4,7,9,10];
+const nnb = (num)=>{
+    console.log(num);
+}
+arr.forEach(nnb);
