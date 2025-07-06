456f2f234e03de3b28acf21df4067c01c83780b2
7starmohit <67998164+7starmohit@users.noreply.github.com>
2025-07-06 11:31:03 +0530

    Create constructor.js

---
+++ constructor.js
@@ -0,0 +1,12 @@
+class binny {
+    constructor(bug) {
+        this.bug = bug;
+        console.log("they are playing" + bug);
+    }
+ }
+ class dady extends binny{
+    
+ }
+
+let d1 = new dady("shiba inu");
+  
